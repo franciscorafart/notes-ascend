055fa18946b72186e091c2378753adf0bf925cba
Christina <christina.brgs@gmail.com>
2024-01-29 23:55:06 -0500

    created get notes endpoint in notes controller

--- notes-app-backend/controllers/Notes.js
+++ notes-app-backend/controllers/Notes.js
@@ -1,2 +1,7 @@
 const notesRouter = require('express').router()
 const Note = require('../models/notesModel')
+
+notesRouter.get('/', async (req, res)=> {
+    const notes = await Note.find({})
+    response.json(notes)
+})
