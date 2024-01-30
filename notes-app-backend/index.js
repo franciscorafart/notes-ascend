9c53c4ccba67ae2c1877a39e8180f2eef1ec3f1b
tyana bandeira <tyanabandeira1@gmail.com>
2024-01-29 20:13:01 -0500

    adding backend files

---
+++ notes-app-backend/index.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const app = express()
+require('express-async-errors')
+const cors = require('cors')
+require('dotenv').config()
+const port = process.env.PORT
+const bodyParser = require('body-parser')
+//const connectDB = require() // create config  folder, connect datatbase file, and then require that file
+//connectDB()
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended:false}))
+const corsOptions = {
+  origin:'*',
+  Credentials: true,
+  optionSuccessStatus: 200,
+}
+app.use(express.json())
+app.use(cors())
+
+app.listen(port, () => console.log(`Port is running on ${port}`))
