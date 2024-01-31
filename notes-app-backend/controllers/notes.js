4e3e1b14dbbe5ee42c0f27b98312781dbb3e8105
Christina <christina.brgs@gmail.com>
2024-01-30 19:13:29 -0500

    created create endpoint

--- notes-app-backend/controllers/notes.js
+++ notes-app-backend/controllers/notes.js
@@ -1,7 +1,20 @@
 const notesRouter = require('express').router()
 const Note = require('../models/notesModel')
 
-notesRouter.get('/', async (req, res)=> {
+notesRouter.get('/', async (req, res) => {
     const notes = await Note.find({})
     response.json(notes)
+})
+
+notesRouter.post('/', async (req, res) => {
+    const body = request.content
+
+    const note = new Note({
+        note: body.note,
+        important: body.important,
+    })
+
+    const savedNote = await note.save()
+
+    response.status(201).json(savedNote)
 })
